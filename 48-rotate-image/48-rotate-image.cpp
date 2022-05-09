313e4c344a9afd464b8e1048e2852c532169887d
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-05-09 20:01:21 +0530

    Time: 6 ms (18.57%), Space: 7 MB (92.92%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(arr[i][j],arr[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+            reverse(arr[i].begin(),arr[i].end());
+    }
+};
