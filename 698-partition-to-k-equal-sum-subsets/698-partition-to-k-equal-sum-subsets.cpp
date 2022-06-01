54a9e2733d324a2a165b1628fa2d035e0403f4b1
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-06-01 19:34:51 +0530

    Time: 20 ms (84.12%), Space: 9.3 MB (93.93%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
+    vector<int>visited;
+    int target;
 	bool canPartitionKSubsets(vector<int>& nums, int k) {
 		int sum = 0;
+        if(k==1){
+            return true;
+        }
 		sum = accumulate(nums.begin(), nums.end(), sum);
-		if (nums.size() < k || sum % k) return false;
+		if(nums.size()<k || sum%k)return false;
 
-		vector<int>visited(nums.size(), false);
-		return backtrack(nums, visited, sum / k, 0, 0, k);
+        sort(nums.begin(),nums.end());
+        target=sum/k;
+		visited.resize(nums.size(),false);
+		return backtrack(nums, 0, 0, k);
 	}
 
-	bool backtrack(vector<int>& nums,vector<int>visited, int target, int curr_sum, int i, int k) {
-		if (k == 1 and curr_sum == target) return true;
+	bool backtrack(vector<int>& nums, int curr_sum, int i, int k) {
+        
+        if(k==0){
+            return true;
+        }
+        if (curr_sum == target) 
+            return backtrack(nums, 0, 0, k-1);
+        
+    
+	
+		for (int j = i; j < nums.size(); j++) 
+        {
+			if (!visited[j] && curr_sum + nums[j] <= target)
+            {
+                
+                    visited[j] = true;
+			        if (backtrack(nums, curr_sum + nums[j], j+1, k)) 
+                        return true;
+			        visited[j] = false;
 
-		if (curr_sum == target) return backtrack(nums, visited, target, 0, 0, k-1);
+                if(!curr_sum || curr_sum+nums[i]==target)
+                    return false;
 
-		for (int j = i; j < nums.size(); j++) {
-			if (visited[j] || curr_sum + nums[j] > target) continue;
-
-			visited[j] = true;
-			if (backtrack(nums, visited, target, curr_sum + nums[j], j+1, k)) return true;
-			visited[j] = false;
-		}
-
+            }	
+        }
 		return false;
 	}
 };
