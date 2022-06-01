9e260647b85aeddffb8639c4f99c6dab08c87e1f
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-06-02 02:05:25 +0530

    Time: 159 ms (38.79%), Space: 20.9 MB (42.09%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+     bool valid(int x, int y, int m, int n) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+    int minCost(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector <vector <int>> dp(m, vector <int>(n, INT_MAX));
+        dp[0][0]=0;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+       int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+        while(!q.empty()){
+            auto temp=q.front();
+            q.pop();
+            int x=temp.first;
+            // int y=temp.second;
+           
+            int y=temp.second;
+             int wt=grid[x][y];
+            for(int k=0;k<4;k++){
+                int xx=x+dx[k];
+                int yy=y+dy[k];
+                int val=1;
+                if(k+1==wt)
+                    val=0;
+                if(valid(xx,yy,m,n)==true){
+                    if(val+dp[x][y]<dp[xx][yy]){
+                        q.push({xx,yy});
+                        dp[xx][yy]=dp[x][y]+val;
+                    }
+                    
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
