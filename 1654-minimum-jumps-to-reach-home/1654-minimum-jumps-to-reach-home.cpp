79da2013d780169861fbadd34e7633f4cbefc27f
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-06-06 03:25:32 +0530

    Time: 44 ms (72.32%), Space: 41.8 MB (15.35%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    int dp[7001][2];
+    int solve(int i,int back,int a,int b,int x){
+        
+        if(i==x)return 0;
+        // if(mp.find(i)!=mp.end() || i>x || i<0)return 1e9;
+        if( i < 0 || mp.find(i)!= mp.end() || i>6000 )
+            return 1e9;
+        
+        if(dp[i][back]!=-1)return dp[i][back];
+        // int temp=0;
+        dp[i][back] =1+solve(i+a,0,a,b,x);
+        // int ans2=1e9;
+        if(!back){
+            dp[i][back]=min(dp[i][back], 1+solve(i-b,1,a,b,x));
+        }
+        return dp[i][back];
+        
+        
+        
+    }
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        for(int i = 0 ; i<forbidden.size();i++)
+            mp[forbidden[i]] = 1;
+         memset(dp,-1,sizeof(dp));
+        int ans= solve(0,0,a,b,x);
+        if(ans>1e9)return -1;
+        return ans;
+    }
+};
