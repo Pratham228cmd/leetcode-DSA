98be44234b2364f5c9ebe7ce9b93f2b5d0bd3b2a
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-06-06 00:32:44 +0530

    Time: 82 ms (65.94%), Space: 50.5 MB (9.91%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        int i=0,maxi=0;
+        while(i<n and i<=maxi){
+            maxi=max(maxi,i+nums[i]);
+            i++;
+        }
+        return i==nums.size();
+        
+    }
+};
