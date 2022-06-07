ad4157fbe87552052a7b9f41f39c22bda57e9791
Pratham228cmd <76724763+Pratham228cmd@users.noreply.github.com>
2022-06-07 20:49:35 +0530

    Time: 274 ms (17.44%), Space: 47.4 MB (9.99%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -3,11 +3,12 @@ public:
   int helper(int n,int k,vector<vector<int>> &dp){
       if(n==0 || n==1)return n;
       if(k==1)return n;
-      if(dp[k][n]!=-1)return dp[k][n];
-      int ans=INT_MAX;int l=1,h=n;
+      if(dp[n][k]!=-1)return dp[n][k];
+      int ans=INT_MAX;
+      int l=1,h=n;
       int temp=0;
       while(h>=l){
-          int mid=(l+h)/2;
+          int mid=(h+l)/2;
           int left=helper(mid-1,k-1,dp);
           int right=helper(n-mid,k,dp);
           temp=1+max(left,right);
@@ -17,11 +18,10 @@ public:
           else h=mid-1;
           ans=min(ans,temp);
       }
-     return dp[k][n]=ans;
-      
+      return dp[n][k]=ans;
   }
     int superEggDrop(int k, int n) {
-         vector<vector<int>> dp(k+1,vector<int> (n+1,-1));
+         vector<vector<int>> dp(n+1,vector<int> (k+1,-1));
         return helper(n,k,dp);
     }
 };
